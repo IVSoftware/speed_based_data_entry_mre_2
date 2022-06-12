a8ee39acf1325357c117566635948caa4009d9c9
Tom Gregor <tgregor@ivsoftware.com>
2022-06-12 06:09:35 -0600

    Add project files.

---
+++ WindowsFormsApp4/MainForm.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.btnWorkaround = new System.Windows.Forms.CheckBox();
+            this.buttonABC = new System.Windows.Forms.Button();
+            this.buttonCDE = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgv
+            // 
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(0, 0);
+            this.dgv.Name = "dgv";
+            this.dgv.RowHeadersWidth = 62;
+            this.dgv.RowTemplate.Height = 28;
+            this.dgv.Size = new System.Drawing.Size(800, 383);
+            this.dgv.TabIndex = 0;
+            // 
+            // btnWorkaround
+            // 
+            this.btnWorkaround.Appearance = System.Windows.Forms.Appearance.Button;
+            this.btnWorkaround.Location = new System.Drawing.Point(12, 403);
+            this.btnWorkaround.Name = "btnWorkaround";
+            this.btnWorkaround.Size = new System.Drawing.Size(207, 40);
+            this.btnWorkaround.TabIndex = 1;
+            this.btnWorkaround.TabStop = false;
+            this.btnWorkaround.Text = "Workaround is OFF";
+            this.btnWorkaround.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnWorkaround.UseVisualStyleBackColor = true;
+            this.btnWorkaround.CheckedChanged += new System.EventHandler(this.btnWorkaround_CheckedChanged);
+            // 
+            // buttonABC
+            // 
+            this.buttonABC.Location = new System.Drawing.Point(225, 403);
+            this.buttonABC.Name = "buttonABC";
+            this.buttonABC.Size = new System.Drawing.Size(75, 40);
+            this.buttonABC.TabIndex = 2;
+            this.buttonABC.TabStop = false;
+            this.buttonABC.Text = "ABC";
+            this.buttonABC.UseVisualStyleBackColor = true;
+            this.buttonABC.Click += new System.EventHandler(this.buttonABC_Click);
+            // 
+            // buttonCDE
+            // 
+            this.buttonCDE.Location = new System.Drawing.Point(306, 403);
+            this.buttonCDE.Name = "buttonCDE";
+            this.buttonCDE.Size = new System.Drawing.Size(75, 40);
+            this.buttonCDE.TabIndex = 3;
+            this.buttonCDE.TabStop = false;
+            this.buttonCDE.Text = "CDE";
+            this.buttonCDE.UseVisualStyleBackColor = true;
+            this.buttonCDE.Click += new System.EventHandler(this.buttonCDE_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(670, 403);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(118, 40);
+            this.buttonClear.TabIndex = 4;
+            this.buttonClear.TabStop = false;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonCDE);
+            this.Controls.Add(this.buttonABC);
+            this.Controls.Add(this.btnWorkaround);
+            this.Controls.Add(this.dgv);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Main Form";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.CheckBox btnWorkaround;
+        private System.Windows.Forms.Button buttonABC;
+        private System.Windows.Forms.Button buttonCDE;
+        private System.Windows.Forms.Button buttonClear;
+    }
+}
+
---
+++ WindowsFormsApp4/MainForm.cs
@@ -0,0 +1,385 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            Load += Form1_Load;
+            dgv.EditingControlShowing += onEditingControlShowing;
+            dgv.CellBeginEdit += onCellBeginEdit;
+            dgv.CellEndEdit += onCellEndEdit;
+            dgv.CurrentCellChanged += onCurrentCellChanged;
+            dgv.CurrentCellDirtyStateChanged += onCurrentCellDirtyStateChanged;
+        }
+
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+        }
+
+        private void onCurrentCellDirtyStateChanged(object sender, EventArgs e)
+        {
+            Debug.WriteLine($"{nameof(onCurrentCellDirtyStateChanged)} {dgv.IsCurrentCellDirty} {dgv.IsCurrentCellInEditMode}");
+        }
+
+        private void onCurrentCellChanged(object sender, EventArgs e)
+        {
+            if(dgv.CurrentCell == null)
+            {
+                Debug.WriteLine($"{nameof(onCurrentCellChanged)} null");
+            }
+            else
+            {
+                Debug.WriteLine($"{nameof(onCurrentCellChanged)} [{dgv.CurrentCell.ColumnIndex}, {dgv.CurrentCell.RowIndex}]");
+            }
+        }
+
+        private void onCellBeginEdit(object sender, DataGridViewCellCancelEventArgs e)
+        {
+            Debug.WriteLine(nameof(onCellBeginEdit));
+        }
+
+        private void onCellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            Debug.WriteLine(nameof(onCellEndEdit));
+        }
+
+        private void onEditingControlShowing(object sender, DataGridViewEditingControlShowingEventArgs e)
+        {
+            if(e.Control is DataGridViewComboBoxEditingControl cbEdit)
+            {
+                CBEdit = cbEdit;
+            }
+        }
+        DataGridViewComboBoxEditingControl _cbEdit = null;
+        public DataGridViewComboBoxEditingControl CBEdit
+        {
+            get => _cbEdit;
+            set
+            {
+                if(!ReferenceEquals(_cbEdit, value))
+                {
+                    if(_cbEdit != null)
+                    {
+                        _cbEdit.GotFocus -= onCBEdit_GotFocus;
+                        _cbEdit.LostFocus -= onCBEdit_LostFocus;
+                        _cbEdit.KeyDown -= _cbEdit_KeyDown;
+                        _cbEdit.KeyUp -= _cbEdit_KeyUp;
+                        _cbEdit.PreviewKeyDown -= _cbEdit_PreviewKeyDown;
+                    }
+                    _cbEdit = value;
+                    if (_cbEdit != null)
+                    {
+                        _cbEdit.GotFocus += onCBEdit_GotFocus;
+                        _cbEdit.LostFocus += onCBEdit_LostFocus;
+                        _cbEdit.KeyDown += _cbEdit_KeyDown;
+                        _cbEdit.KeyUp += _cbEdit_KeyUp;
+                        _cbEdit.PreviewKeyDown += _cbEdit_PreviewKeyDown;
+                    }
+                }
+            }
+        }
+
+        private void _cbEdit_KeyUp(object sender, KeyEventArgs e)
+        {
+            Debug.WriteLine($"Key up '{e.KeyData}'");
+        }
+
+        private void _cbEdit_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            // Tab is not an input character by default so hook it here.
+            if(e.KeyData == Keys.Tab)
+            {
+                Debug.WriteLine($"Key down '{e.KeyData}'");
+            }
+        }
+
+        private void _cbEdit_KeyDown(object sender, KeyEventArgs e)
+        {
+            Debug.WriteLine($"Key down '{e.KeyData}'");
+        }
+
+        private void onCBEdit_GotFocus(object sender, EventArgs e)
+        {
+            Debug.WriteLine($"CBEdit got focus with text='{_cbEdit.Text}'");
+        }
+
+        private void onCBEdit_LostFocus(object sender, EventArgs e)
+        {
+            Debug.WriteLine($"CBEdit losing focus with text='{_cbEdit.Text}'");
+        }
+#if true
+        DataTable dtString1;
+        DataTable dtString2;
+        DataTable dtString3;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // create three combobox columns and put them side-by-side:
+            // first column:
+            DataGridViewComboBoxColumn dgvcbc1 = new DataGridViewComboBoxColumn();
+            dgvcbc1.DataPropertyName = "String1";
+            dgvcbc1.Name = "String1";
+
+            dtString1 = new DataTable("String1Options");
+            dtString1.Columns.Add("String1Long", typeof(string));
+
+            dtString1.Rows.Add(String.Empty);   // Add this line
+            dtString1.Rows.Add("apple");
+            dtString1.Rows.Add("bob");
+            dtString1.Rows.Add("clobber");
+            dtString1.Rows.Add("dilbert");
+            dtString1.Rows.Add("ether");
+
+            dgv.Columns.Insert(0, dgvcbc1);
+
+            dgvcbc1.DisplayMember = dtString1.Columns[0].ColumnName;
+            dgvcbc1.ValueMember = dtString1.Columns[0].ColumnName;
+            dgvcbc1.DataSource = dtString1;
+
+            dgvcbc1.FlatStyle = FlatStyle.Flat;
+
+            // create the second column:
+            DataGridViewComboBoxColumn dgvcbc2 = new DataGridViewComboBoxColumn();
+            dgvcbc2.DataPropertyName = "String2";
+            dgvcbc2.Name = "String2";
+
+            dtString2 = new DataTable("String2Options");
+            dtString2.Columns.Add("String2Long", typeof(string));
+
+            dtString2.Rows.Add(String.Empty);   // Add this line
+            dtString2.Rows.Add("apple");
+            dtString2.Rows.Add("bob");
+            dtString2.Rows.Add("clobber");
+            dtString2.Rows.Add("dilbert");
+            dtString2.Rows.Add("ether");
+
+            dgv.Columns.Insert(1, dgvcbc2);
+
+            dgvcbc2.DisplayMember = dtString2.Columns[0].ColumnName;
+            dgvcbc2.ValueMember = dtString2.Columns[0].ColumnName;
+            dgvcbc2.DataSource = dtString2;
+
+            dgvcbc2.FlatStyle = FlatStyle.Flat;
+
+            // create the third column:
+            DataGridViewComboBoxColumn dgvcbc3 = new DataGridViewComboBoxColumn();
+            dgvcbc3.DataPropertyName = "String3";
+            dgvcbc3.Name = "String3";
+
+            dtString3 = new DataTable("String3Options");
+            dtString3.Columns.Add("String3Long", typeof(string));
+
+            dtString3.Rows.Add(String.Empty);   // Add this line
+            dtString3.Rows.Add("apple");
+            dtString3.Rows.Add("bob");
+            dtString3.Rows.Add("clobber");
+            dtString3.Rows.Add("dilbert");
+            dtString3.Rows.Add("ether");
+
+            dgv.Columns.Insert(2, dgvcbc3);
+
+            dgvcbc3.DisplayMember = dtString3.Columns[0].ColumnName;
+            dgvcbc3.ValueMember = dtString3.Columns[0].ColumnName;
+            dgvcbc3.DataSource = dtString3;
+
+            dgvcbc3.FlatStyle = FlatStyle.Flat;
+        }
+#else
+        // During debugging I reworded this to make sure I
+        // understood what was supposed to be happening here.
+        string[] values { get; } = new string[]
+        {
+            "",
+            "apple",
+            "bob",
+            "clobber",
+            "dilbert",
+            "ether"
+        };
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // create three combobox columns and put them side-by-side:
+
+            string colName;
+            DataTable options;
+            DataGridViewColumn c;
+
+            for (int i = 1; i <= 3; i++)
+            {
+                colName = $"String{i}";
+                options = new DataTable();
+                options.Columns.Add(colName, typeof(string));
+                foreach (var value in values)
+                {
+                    options.Rows.Add(value);
+                }
+                c = new DataGridViewComboBoxColumn()
+                {
+                    Name = colName,
+                    DataPropertyName = colName,
+                    DisplayMember = options.Columns[0].ColumnName,
+                    ValueMember = options.Columns[0].ColumnName,
+                    DataSource = options,
+                    FlatStyle = FlatStyle.Flat,
+                };
+                dgv.Columns.Add(c);
+            }
+        }
+#endif
+
+        SemaphoreSlim _criticalSection = new SemaphoreSlim(1, 1);
+        public void SendKeyPlusTab(string keys)
+        {
+            if (_criticalSection.Wait(0))
+            {
+                try
+                {
+                    var nRowsB4 = dgv.Rows.Count;
+                    if (!dgv.Focused)
+                    {
+                        dgv.Focus();
+                    }
+                    bool first = true;
+                    foreach (var key in keys)
+                    {
+                        SendKeys.SendWait($"{key}\t");
+                        if (first)
+                        {
+                            // Using SendKeys acts slightly different internally from
+                            // pressing and releasing physical keys. If the button
+                            // clicks are too fast, a new row might not create the way
+                            // it should. This line ensures the call value changes.
+                            // =======================================================
+                            // Force new row - fixes an artifact of automated testing.
+                            first = false;
+                            dgv.CurrentCell.Value = CBEdit.Text;
+                        }
+                    }
+                    if (dgv.Rows.Count == nRowsB4)
+                    {
+                        MessageBox.Show(msg);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debug.Assert(false, ex.Message);
+                }
+                finally
+                {
+                    _criticalSection.Release();
+                }
+            }
+        }
+        const string msg =
+@"New bug detected!
+
+The DGV should make a row as soon as a cell 
+goes into edit mode. This only seems to
+occur when repeating the same button.
+        
+Alternating the ABC and CDE buttons seems
+to work always.";
+
+        private void buttonABC_Click(object sender, EventArgs e)
+        {
+            SendKeyPlusTab("abc");
+        }
+
+        private void buttonCDE_Click(object sender, EventArgs e)
+        {
+            SendKeyPlusTab("cde");
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            dgv.Rows.Clear();
+        }
+
+        private void btnWorkaround_CheckedChanged(object sender, EventArgs e)
+        {
+            if(btnWorkaround.Checked)
+            {
+                btnWorkaround.Text = "Workaround is ON";
+            }
+            else
+            {
+                btnWorkaround.Text = "Workaround is OFF";
+            }
+        }
+    }
+
+#if false
+**N O R M A L  (slow)**
+
+    onCurrentCellChanged [0, 0]
+    onCellBeginEdit
+    CBEdit got focus with text='apple'
+    Key down 'A'
+    onCurrentCellDirtyStateChanged True True
+    Key down 'Tab'
+    onCurrentCellDirtyStateChanged False True
+    CBEdit losing focus with text='apple'
+    onCellEndEdit
+    onCurrentCellChanged [1, 0]
+    onCellBeginEdit
+    CBEdit got focus with text='apple'
+    Key down 'B'
+    onCurrentCellDirtyStateChanged True True
+    Key down 'Tab'
+    onCurrentCellDirtyStateChanged False True
+    CBEdit losing focus with text='bob'
+    onCellEndEdit
+    onCurrentCellChanged [2, 0]
+    onCellBeginEdit
+    CBEdit got focus with text='apple'
+    Key down 'C'
+    onCurrentCellDirtyStateChanged True True
+    Key down 'Tab'
+    onCurrentCellDirtyStateChanged False True
+    CBEdit losing focus with text='clobber'
+    onCellEndEdit
+    onCurrentCellChanged [0, 1]
+
+**P A T H O L O G I C A L (fast)**
+    
+    onCurrentCellChanged [0, 1]
+    onCellBeginEdit
+    CBEdit got focus with text='apple'
+    Key down 'A'
+    onCurrentCellDirtyStateChanged True True
+    Key down 'Tab'
+    onCurrentCellDirtyStateChanged False True
+    CBEdit losing focus with text='apple'
+    onCellEndEdit
+    onCurrentCellChanged [1, 1]
+    onCellBeginEdit
+    CBEdit got focus with text='bob'
+    Key down 'B'
+    // Missing event (Insufficient time for list search.)
+    Key down 'Tab'
+    CBEdit losing focus with text='bob'
+    onCellEndEdit
+    onCurrentCellChanged [2, 1]
+    onCellBeginEdit
+    CBEdit got focus with text='clobber'
+    Key down 'C'
+    // Missing event (Insufficient time for list search.)
+    Key down 'Tab'
+    CBEdit losing focus with text='clobber'
+    onCellEndEdit
+    onCurrentCellChanged [0, 2]
+#endif
+}
